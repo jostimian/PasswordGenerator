050349be241ea39823aa778db03ec60f9046f2f6
Timothy Ganoza <jostimian@gmail.com>
2020-08-28 10:19:57 +0800

    New Symbols Are Added

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,23 +1,24 @@
 from tkinter import *
 import random
 root = Tk()
-root.title("Password Generator V1.0")
+root.title("Password Generator V1.1")
 #passwordChoices
 lowerCases = "abcdefghijklmnopqrstuvwxyz"
 uperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 symbols = "!@#$%^&*()_+-="
 numbers = "1234567890"
+IDN_Remapped_Case_Atomic = "АӘӔБВГҐҒҔДԀЂԂҘЕЄЖҖЗԄЅӠԆИҊІЈКҚӃҠҞҜЛӅЉԈМӍНӉҢӇҤЊԊОӨПҦҀРҎСԌҪТԎҬЋУҮҰѸФХҲҺѠѾѼѺЦҴЧҶӋҸҼҾЏШЩЪЫЬҌѢЭЮЯѤѦѪѨѬѮѰѲѴҨ"
 #Generate Password 
 def genpas():
 	passlen = int(length.get())
-	combine = lowerCases + uperCases + symbols + numbers
+	combine = lowerCases + uperCases + symbols + numbers + IDN_Remapped_Case_Atomic	
 	generated_password = "".join([random.choice(combine) for _ in range(passlen)] )
 	gendpass.delete(0,END)
 	gendpass.insert(0,generated_password)
 #Tkinter Widgets
 ttl = Label(text = "Password Generator V1.0")
 ttl.pack()
-ttl.config(font=("Calibri", 44))
+ttl.config(font=("Calibri", 44)) 
 
 lenlbl = Label(text = "Password length")
 lenlbl.pack()
