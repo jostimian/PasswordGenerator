bd0e01e92fd5e8eee841f94f005a7bad74d45f2c
Timothy Ganoza <jostimian@gmail.com>
2020-08-30 18:01:41 +0800

    Code Changes

--- Bin/passwordGenerator.py
+++ Bin/passwordGenerator.py
@@ -2,16 +2,18 @@ from tkinter import *
 import random
 root = Tk()
 root.title("Password Generator V1.1")
+
 #passwordChoices
 lowerCases = "abcdefghijklmnopqrstuvwxyz"
 uperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 symbols = "!@#$%^&*()_+-="
 numbers = "1234567890"
-IDN_Remapped_Case_Atomic = "АӘӔБВГҐҒҔДԀЂԂҘЕЄЖҖЗԄЅӠԆИҊІЈКҚӃҠҞҜЛӅЉԈМӍНӉҢӇҤЊԊОӨПҦҀРҎСԌҪТԎҬЋУҮҰѸФХҲҺѠѾѼѺЦҴЧҶӋҸҼҾЏШЩЪЫЬҌѢЭЮЯѤѦѪѨѬѮѰѲѴҨ®©"
-combine = lowerCases + uperCases + symbols + numbers + IDN_Remapped_Case_Atomic
+normal = lowerCases + uperCases + numbers 
+special = normal + symbols
 mvar = IntVar()
 mvar2 = IntVar()
 iscomplex = False
+
 #Generate Password 
 def compx_savetofile():
 	global iscomplex
@@ -20,9 +22,9 @@ def compx_savetofile():
 	if iscomplex == True:
 		iscomplex = False
 def genpas():
-	if iscomplex == True: #
+	if iscomplex == True: 
 		passlen = int(length.get())*2
-		phase1 = "".join([random.choice(combine) for _ in range (passlen)])
+		phase1 = "".join([random.choice(special) for phase_1 in range (passlen)])
 		phase1_len = int(len(phase1)/2)
 		generated_password = "".join([random.choice(phase1) for _ in range (phase1_len)])
 		gendpass.delete(0,END)
@@ -30,9 +32,10 @@ def genpas():
 
 	else:
 		passlen = int(length.get())	
-		generated_password = "".join([random.choice(combine) for _ in range(passlen)] )
+		generated_password = "".join([random.choice(normal) for _ in range(passlen)] )
 		gendpass.delete(0,END)
 		gendpass.insert(0,generated_password)
+
 #Tkinter Widgets
 ttl = Label(text = "Password Generator V1.1")
 ttl.pack()
@@ -52,7 +55,7 @@ gen.config(font=("Calibri",18))
 gendpass = Entry()
 gendpass.pack(ipadx=100,ipady=10)
 
-compl = Checkbutton(text = "Make It More Complex!", state = ACTIVE, variable = mvar,command = compx_savetofile)
+compl = Checkbutton(text = "Add Special Characters", state = ACTIVE, variable = mvar,command = compx_savetofile)
 compl.pack()
 
 submit = Button(text = "Generate Secure Password", command = genpas)
